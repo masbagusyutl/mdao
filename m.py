f88acd0846b4cb2ea8a353295249b29231cb8adb
masbagusyutl <82576353+masbagusyutl@users.noreply.github.com>
2024-07-19 19:35:46 +0700

    Update m.py

--- m.py
+++ m.py
@@ -49,7 +49,27 @@ def update_telegram_init_data(telegram_init_data):
 def claim_rewards(telegram_init_data):
     url = 'https://zavod-api.mdaowallet.com/user/claim'
     headers = {
-        'Telegram-Init-Data': telegram_init_data
+        ':authority': 'zavod-api.mdaowallet.com',
+        ':method': 'POST',
+        ':path': '/user/claim',
+        ':scheme': 'https',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Content-Length': '0',
+        'Origin': 'https://zavod.mdaowallet.com',
+        'Pragma': 'no-cache',
+        'Priority': 'u=1, i',
+        'Referer': 'https://zavod.mdaowallet.com/',
+        'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Microsoft Edge";v="126", "Microsoft Edge WebView2";v="126"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'Telegram-Init-Data': telegram_init_data,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'
     }
     response = requests.post(url, headers=headers)
     return response.status_code
